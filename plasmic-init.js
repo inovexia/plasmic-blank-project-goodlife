254cb247da3fa250e21dc01d5135346e3519a21a
Rajkumar <50763437+rajkumar-inovage@users.noreply.github.com>
2026-01-03 21:40:41 +0530

    added "Starter","New Billybishop"

--- plasmic-init.js
+++ plasmic-init.js
@@ -4,9 +4,19 @@ import { initPlasmicLoader } from '@plasmicapp/loader-nextjs';
 export const PLASMIC = initPlasmicLoader({
   projects: [
     {
-      id: 'mQc8YBkhvPH6vABbdioLrq', // ID of a project you are using
+      id: 'mQc8YBkhvPH6vABbdioLrq',
       token:
-        'HIyVFqQ10HyKLIT9WhQgyjdyi23053KDt6jqNIlGTntApj6j1FtfgflORTVqeA7WlSczgqA1afaKlOLxqmQ', // API token for that project
+        'HIyVFqQ10HyKLIT9WhQgyjdyi23053KDt6jqNIlGTntApj6j1FtfgflORTVqeA7WlSczgqA1afaKlOLxqmQ',
+    },
+    {
+      id: 'uMs74eyiqRaLCuDw5CrYJD',
+      token:
+        'cp2foma1MdcTCVVP0LID24gTy3dVbhcQ7czulGPfbQrlrM5en1szbJC3Cnhp8ZAXJaCUJmHAFqSWUyDymiebA',
+    },
+    {
+      id: 'ntpcpEcLXuAagozZD2zkQJ',
+      token:
+        '3CGl8KyHYi09ysM4VAefsb1gsBKDcgOiRBSkry7f99Dyr5Qazp4HGg8j0KfEQnUIVFf2n0lyp0d2E6VfldqQ',
     },
   ],
   // Fetches the latest revisions, whether or not they were unpublished!
